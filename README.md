9a6a408e99a4a6eb6a0faa7010e146a4c4599f50
MarcoPOtard <30588375+MarcoPOtard@users.noreply.github.com>
2024-08-29 15:09:49 +0200

    New site astro (#9)
    * initial commit new site with Astro and Starfunnel template
    
    * Font Family / Theme Colors / Matou Logos
    
    * Images
    
    * Clean useless blocks
    Rename FAQ to Process
    ImageTextSection and ColoredText Astro components
    Refacto
    
    * Update favicon
    
    * Add Matou logo
    
    * Add icons and pictures
    
    * Delete block ImageTextSection and Add block MultiCards
    
    * Update and Add colors
    
    * Update home page and content
    
    * Delete unused pages
    
    * Add legal pages
    
    * Delete unused codes and pictures
    
    * Update home page content
    
    * Update legal pages content
    
    * Update homepage and Delete useless element on BaseLayout
    
    * Add meta on homepage
    
    * Update email adress and Delete contact form access
    
    * Update Readme
    
    * Update footer
    
    * Fix error on favicons
    
    * Add parameters to deploy on github fom main
    
    * Fix link and anchor on navigations
    
    * PR Review
    
    * PR Review
    
    * Update breakpoints styling
    
    ---------
    
    Co-authored-by: Rcaradec <remi.caradec@matou-technologies.fr>

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# matou-technologies.fr
+# Matou technologies
+
+Matou is a website based on [Astro](https://astro.build/) and [Starfunnel](https://starfunnel.unfolding.io/) theme.
+
+We used royalty free image and AI generated images [unsplash](https://unsplash.com/fr) et [freepik](https://fr.freepik.com/).
+
+We used icons from [react-icons](https://react-icons.github.io/react-icons/).
+
+## Development
+
+For the local development
+
+### 1. Install dependencies
+
+```bash
+npm install
+```
+
+### 2. Start Development server
+
+```bash
+npm run dev
+```
+
+### 3. To build the production site to `./dist`
+
+```bash
+npm run build
+```
+
+### 4. To deploy
+
+Merge an main branch on Github
---
+++ astro.config.mjs
@@ -0,0 +1,86 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import m2dx from "astro-m2dx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import rehypeExternalLinks from "rehype-external-links";
+import fauxRemarkEmbedder from "@remark-embedder/core";
+import fauxOembedTransformer from "@remark-embedder/transformer-oembed";
+import { loadEnv } from "vite";
+const remarkEmbedder = fauxRemarkEmbedder.default;
+const oembedTransformer = fauxOembedTransformer.default;
+import vue from "@astrojs/vue";
+/** @type {import('astro-m2dx').Options} */
+import icon from "astro-icon";
+import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
+
+const env = loadEnv("", process.cwd(), "NETLIFY");
+
+const m2dxOptions = {
+  exportComponents: true,
+  unwrapImages: true,
+  autoImports: true,
+};
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://www.matou-technologies.fr",
+  output: "hybrid",
+  adapter: env.NETLIFY ? netlify() : vercel(), // vercel() or netlify()
+  integrations: [
+    icon(),
+    mdx({}),
+    sitemap(),
+    tailwind(),
+    vue({
+      appEntrypoint: "/src/pages/_app",
+    }),
+  ],
+  markdown: {
+    extendDefaultPlugins: true,
+    remarkPlugins: [
+      [
+        remarkEmbedder,
+        {
+          transformers: [oembedTransformer],
+        },
+      ],
+      [m2dx, m2dxOptions],
+    ],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          rel: ["nofollow"],
+          target: ["_blank"],
+        },
+      ],
+    ],
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        external: [],
+      },
+      assetsInlineLimit: 10096,
+    },
+    ssr: {
+      // Workaround until they support native ESM
+      noExternal: ["vue3-popper"],
+    },
+  },
+  build: {
+    inlineStylesheets: "always",
+  },
+  prefetch: {
+    defaultStrategy: "viewport",
+  },
+  scopedStyleStrategy: "attribute",
+  image: {
+    cache: true,
+    service: {
+      entrypoint: "./src/image-service/image-service",
+    },
+  },
+});
