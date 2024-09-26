ad3b198f93bc7c79c2033ec47b0d08958304cc44
MarcoPOtard <30588375+MarcoPOtard@users.noreply.github.com>
2024-09-26 12:29:03 +0200

    Add Blog (#37)

--- src/content/config.ts
+++ src/content/config.ts
@@ -29,7 +29,24 @@ const teamCollection = defineCollection({
   }),
 });
 
+const blogCollection = defineCollection({
+  schema: z.object({
+    draft: z.boolean(),
+    title: z.string(),
+    snippet: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+    publishDate: z.coerce.date(),
+    author: z.string().default("Astroship"),
+    category: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
+
 export const collections = {
   recruitment: recruitmentCollection,
   team: teamCollection,
+  blog: blogCollection,
 };
